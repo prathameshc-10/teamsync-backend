d4238bd95a5ed7b3ae022466376dbcbb7e8a4387
Anjali Avasthi <157029346+Anjali03-avasthi@users.noreply.github.com>
2026-04-08 12:30:53 +0530

    org messages resolved

--- src/services/organization.services.ts
+++ src/services/organization.services.ts
@@ -1,4 +1,5 @@
 import prisma from '../config/prisma';
+import { ORG_MESSAGES } from '../constants/messages';
 
 export const createOrganization = async (orgName: string, userId: number) => {
   return await prisma.organization.create({
@@ -35,10 +36,10 @@ export const getMembers = async (orgId: number) => {
 
 export const addMember = async (orgId: number, userId: number) => {
   const loginExists = await prisma.login.findUnique({ where: { userId } });
-  if (!loginExists) throw { status: 404, message: 'User not found' };
+  if (!loginExists) throw { status: 404, message: ORG_MESSAGES.ERROR.USER_NOT_FOUND  };
 
   const alreadyMember = await prisma.user.findFirst({ where: { userId, orgId } });
-  if (alreadyMember) throw { status: 409, message: 'User already in org' };
+  if (alreadyMember) throw { status: 409,  message: ORG_MESSAGES.ERROR.ALREADY_MEMBER};
 
   return await prisma.user.create({ data: { userId, orgId } });
 };
